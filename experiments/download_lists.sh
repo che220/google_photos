9984b216fda0adebda5a7a2f100dc56634803c4c
Your Name <you@example.com>
2021-09-06 12:41:53 -0500

    updated download script

--- experiments/download_lists.sh
+++ experiments/download_lists.sh
@@ -2,19 +2,29 @@
 
 . $HOME/venvs/venv_w2/bin/activate
 
+scriptDir=$(dirname $0)
+scriptDir=$(cd $scriptDir; pwd)
+echo "script dir: $scriptDir"
+
 cmd="python $HOME/projects/google_photos/media_items.py"
 
 function get_list()
 {
     local user=$1
     echo "get list of user $user ..."
-    rm -f photo_list.csv
-    ln -s -f token_photoslibrary_v1_${user}.pickle token_photoslibrary_v1.pickle
-    $cmd
-    mv -vf photo_list.csv photo_list_${user}.csv
+    local token_file=$scriptDir/token_photoslibrary_v1_${user}.pickle
+    local list_file=$scriptDir/photo_list_${user}.csv
+    if [[ -e $list_file ]]; then
+	echo "$list_file exists. Skip downloading list"
+	return
+    fi
+    ls -l token_photos* photo_list*
+    echo
+    run_cmd="$cmd $token_file $scriptDir $list_file"
 }
 
 users="920133 hw libin"
+users="920133 hw"
 for u in $users; do
     get_list $u
 done
@@ -23,10 +33,16 @@ echo "All lists are downloaded"
 function download_items()
 {
     local user=$1
-    echo "download items of user $user ..."
-    ln -s -f token_photoslibrary_v1_${user}.pickle token_photoslibrary_v1.pickle
-    ln -s -f photo_list_${user}.csv photo_list.csv
-    $cmd
+    echo "get list of user $user ..."
+    local token_file=$scriptDir/token_photoslibrary_v1_${user}.pickle
+    local list_file=$scriptDir/photo_list_${user}.csv
+    if [[ ! -e $list_file ]]; then
+	echo "$list_file does not exist. Skip downloading photos"
+	return
+    fi
+    ls -l token_photos* photo_list*
+    echo
+    run_cmd="$cmd $token_file $scriptDir $list_file"
 }
 
 for u in $users; do
